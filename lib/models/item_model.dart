6de758c599c405a599a76a50f4c8407953079897
pruch10 <pruch10@gmail.com>
2022-05-04 12:54:50 +0200

    add date difference

--- lib/models/item_model.dart
+++ lib/models/item_model.dart
@@ -1,13 +1,17 @@
-class ItemModel {
-  final String id;
-  final String title;
-  final String imageURL;
-  final int releaseDate;
 
+class ItemModel {
   ItemModel({
     required this.id,
     required this.title,
     required this.imageURL,
     required this.releaseDate,
   });
+  final String id;
+  final String title;
+  final String imageURL;
+  final DateTime releaseDate;
+
+  String daysLeft() {
+    return releaseDate.difference(DateTime.now()).inDays.toString();
+  }
 }
